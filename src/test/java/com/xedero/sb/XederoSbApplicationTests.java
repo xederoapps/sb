f164f58d9af51ebf6941e8ce1a548a074c49ec19
Andrew BK <Andrew@Andrew-MacBookPro.local>
2021-11-21 09:45:44 +0900

    Initial commit

---
+++ src/test/java/com/xedero/sb/XederoSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xedero.sb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XederoSbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
